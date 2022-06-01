c18ec9cae401d25a7c85c3bde72949f7830db44e
pintard <donovanpintard@gmail.com>
2022-06-01 19:17:42 -0400

    small change

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -5,37 +5,27 @@ def arithmetic_arranger(problems, answer=False):
     MARGIN_SPACE = "    "
     SINGLE_SPACE = " "
     SEPARATOR_CHAR = "-"
-
-    first_operands, second_operands, operators = [], [], []
     first_line, second_line, third_line, fourth_line = [], [], [], []
 
     for problem in problems:
         [first, operator, second] = problem.split(' ')
-        first_operands.append(first)
-        operators.append(operator)
-        second_operands.append(second)
 
-    for i in range(len(operators)):
-        if not (first_operands[i].isdigit() and second_operands[i].isdigit()):
+        if not (first.isdigit() and second.isdigit()):
             return "Error: Numbers must only contain digits."
-        if len(first_operands[i]) > 4 or len(second_operands[i]) > 4:
+        if len(first) > 4 or len(second) > 4:
             return "Error: Numbers cannot be more than four digits."
-        if not ("+" in operators[i] or "-" in operators[i]):
+        if not ("+" in operator or "-" in operator):
             return "Error: Operator must be \'+\' or \'-\'."
 
-        ans = str(eval(first_operands[i] + operators[i] + second_operands[i]))
-
-        len_1 = len(first_operands[i])
-        len_2 = len(second_operands[i])
-        len_3 = len(ans)
-
+        ans = str(eval(first + operator + second))
+        len_1, len_2, len_3 = len(first), len(second), len(ans)
         repeat_1 = 2 if len_1 > len_2 else ((len_2 - len_1) + 2)
         repeat_2 = ((len_1 - len_2) if len_1 > len_2 else 0) + 1
         repeat_3 = max((len_1 + repeat_1), (len_2 + repeat_2 + 1))
         repeat_4 = repeat_3 - len_3
 
-        first_line.append(SINGLE_SPACE * repeat_1 + first_operands[i])
-        second_line.append(operators[i] + SINGLE_SPACE * repeat_2 + second_operands[i])
+        first_line.append(SINGLE_SPACE * repeat_1 + first)
+        second_line.append(operator + SINGLE_SPACE * repeat_2 + second)
         third_line.append(SEPARATOR_CHAR * repeat_3)
         fourth_line.append(SINGLE_SPACE * repeat_4 + ans)
 
